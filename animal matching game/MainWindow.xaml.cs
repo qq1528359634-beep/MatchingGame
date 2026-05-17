4bd20b9778ee4b4b9802518c8563e84d6fe5fbbd
Cks <qq1528359634@gmail.com>
2026-05-17 23:12:40 +0900

    alter some content

--- animal matching game/MainWindow.xaml.cs
+++ animal matching game/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 
 
@@ -55,26 +48,12 @@ namespace MatchingGame
             {
                 "🦜","🦜",
                 "🦖","🦖",
-                /*"🐲","🐲",*/
                 "🐬","🐬",
                 "🐋","🐋",
                 "🦆","🦆",
                 "🐈","🐈",
                 "🦢","🦢",
-               /* "🦌","🦌",*/
                 "🐀","🐀",
-               /* "🦣","🦣",
-                "🐼","🐼",
-                "🐝","🐝",
-                "🦨","🦨",
-                "🦂","🦂",
-                "🐌","🐌",
-                "🕷️","🕷️",
-                "🦞","🦞",
-                "🦀","🦀",
-                "🦇","🦇",
-               
-                "🕊️","🕊️",*/
             };
             Random random = new Random();
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
